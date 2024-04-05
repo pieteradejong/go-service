dc86e47cd187c5d56893c85449aadfcf57befa64
Pieter de Jong <pieter.a.dejong@gmail.com>
2024-04-05 14:59:29 -0400

    feat: add logging; launch tasks in chunks; emojis by emotional state

--- src/utils/simulate_requests.py
+++ src/utils/simulate_requests.py
@@ -1,9 +1,8 @@
 import time
-import json
 import random
 import asyncio
 import aiohttp
-
+import logging
 
 emojis = [
     ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣"],
@@ -15,35 +14,66 @@ emojis = [
     ["🤠", "😇", "🤗", "🤡", "🤥", "🤓", "😈", "👿"],
     ["👋", "🤚", "🖐", "✋", "🖖", "👌", "🤏"],
     ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇"],
-    ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑"]
+    ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑"],
 ]
 
-car_emojis = ["🚗", "🚕", "🚙"]
+emotional_states = {
+    "Happy/Positive": ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣"],
+    "Love/Affection": ["😘", "😗", "😚", "😙", "🥰", "😍", "🤩"],
+    "Excitement/Thrill": ["🤪", "😲", "🤯", "😮"],
+    "Thoughtful/Contemplative": ["🤔", "🤨", "🧐", "🤓"],
+    "Disappointment/Sadness": ["😢", "😭", "🥺", "😦", "😧"],
+    "Anger/Frustration": ["😠", "😡", "🤬"],
+    "Mischievous/Playful": ["😈", "👿", "🤡", "🤠", "😇"],
+}
+
 emojis_flat = [emoji for sublist in emojis for emoji in sublist]
 
 base_url = "http://localhost:8080"
 endpoint = "/reaction"
 full_url = base_url + endpoint
-headers = {'Content-Type': 'application/json'}
+headers = {"Content-Type": "application/json"}
 
 NUM_SIMULATED_CLIENTS = 10_000
 NUM_EMOJI_REACTIONS = 4
+CHUNK_SIZE = 1000
 
-async def simulate_client(session, user_id: int, emoji_list: list) -> None:
+
+def init():
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+
+async def simulate_client(session, user_id: int, emotional_states: dict) -> None:
+    mood = random.choice(list(emotional_states.keys()))
+    emoji_list = emotional_states[mood]
     for _ in range(NUM_EMOJI_REACTIONS):
         emoji = random.choice(emoji_list)
         timestamp = int(time.time())
         payload = {"user_id": user_id, "emoji": emoji, "timestamp": timestamp}
-        async with session.post(full_url, json=payload) as response:
-            print(f'Client {user_id} selected emoji: {emoji}')
-            print(f"Response code: {response.status}, message: await {response.text()}")
-
+        try:
+            async with session.post(full_url, json=payload) as response:
+                logging.info(f"Client {user_id} in {mood} mood selected emoji: {emoji}")
+                logging.info(f"Response code: {response.status}")
+        except aiohttp.ClientError as e:
+            logging.info(f"Request failed for user {user_id}: {str(e)}")
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        tasks = [asyncio.create_task(simulate_client(session, i, emojis_flat)) for i in range(NUM_SIMULATED_CLIENTS)]
-        await asyncio.gather(*tasks)
+        for chunk in range(0, NUM_SIMULATED_CLIENTS, CHUNK_SIZE):
+            tasks = [
+                asyncio.create_task(simulate_client(session, i, emotional_states))
+                for i in range(chunk, min(chunk + 1000, NUM_SIMULATED_CLIENTS))
+            ]
+            await asyncio.gather(*tasks)
 
-asyncio.run(main())
 
+def run():
+    init()
+    asyncio.run(main())
+
+
+if __name__ == "__main__":
+    run()
